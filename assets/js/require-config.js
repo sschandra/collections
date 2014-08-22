f2f24004bf1cdf17330242ee76e0016ac7a4a81f
Satish Chandra <pingto.chandra@gmail.com>
2014-08-23 00:40:18 +0530

    combined branches

---
+++ assets/js/require-config.js
@@ -0,0 +1,26 @@
+/**
+ * Config options at: http://requirejs.org/docs/api.html#config
+ */
+require.config({
+    
+    shim: {
+        
+    },
+
+    paths: {
+        
+
+        
+        // MVC
+        'stapes': 'vendor/stapes/stapes.min',
+        
+        // jQuery
+        'jquery': 'vendor/jquery/dist/jquery.min',
+
+        
+        // jQuery UI core
+        'jquery-ui-core': 'vendor/jquery-ui/jquery-ui.min',
+    },
+
+    waitSeconds: 200
+});
